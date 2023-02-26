57752274e31715471e3993897f697447cbf129ac
Evgeshkaa <107510236+Evgeshkaa@users.noreply.github.com>
2023-02-26 23:31:10 +0300

    feat: reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(number) {
+    let result = 0;
+    number = Math.abs(number);
+    while (number > 0) {
+        result = result * 10 + number % 10;
+        number = Math.floor(number / 10);
+    }
+
+    return result;
 }
